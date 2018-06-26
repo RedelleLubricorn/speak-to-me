62604c58b0e2ad9887fd185757cadb4e4257ef71
Alexei <Gudimenko>
2018-06-26 13:12:08 +1000

    Initial Commit

---
+++ SpeakToMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeakToMe
+//
+//  Created by Alexei Gudimenko on 26/6/18.
+//  Copyright © 2018 Alexei Gudimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
